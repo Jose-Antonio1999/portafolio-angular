4b527fb84a0755094da50e389f1c607dc1410bf7
Jose Antonio <jose.rojascusi@gmail.com>
2021-02-17 06:34:25 +0100

    Fin de ls sección tres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: PortafolioComponent},
+  {path:'about', component: AboutComponent},
+  {path:'item', component: ItemComponent},
+  // redirecionar la pagina si la ruta no se las declaradas
+  {path:'**', pathMatch:'full',redirectTo:'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // se usa useHash para no tener problemas al subir al servidor 
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
